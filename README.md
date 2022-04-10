2a4d6832ce982f897c87fa981162dd1463b14422
KAYOKG <kayo.kg@hotmail.com>
2022-04-10 17:58:55 -0300

    development

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ping-Pong-Game
 
+Final conclusion
+
+-Melhorar design
+-Resolver bug da bola entrar de baixo da plataforma
+
--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ let xBall = 250
 let yBall = 280
 let diameter = 20
 let radius = diameter/2
-let xBallspeed = 6
-let yBallspeed = 6
+var xBallspeed = 6
+var yBallspeed = 6
 
 //CODIGUIN DA PLATAFORMA
 let xPlatform = 212.5
-let yPlatform = 582
+let yPlatform = 584
 let platformwidth = 75
 let platformheight = 15
 let platformcorner = 3
@@ -18,13 +18,16 @@ let xOpponentplatform = 212.5
 let yOpponentplatform = 3
 let opponentplatformspeed 
 
-//let collision = false
+let collision = false
+
+let myscores = 0
+let opponentscores = 0
 
 function setup(){
   createCanvas(500, 600)
 }
 function draw() {
-  background(0)
+  background(35)
   show_the_ball()
   ball_movement()
   edge_check()
@@ -32,11 +35,15 @@ function draw() {
   platform_movement()
   show_opponent_platform()
   opponent_platform_move()
-  collision_check()
-  opponent_collision_check()
-  //collision_checklibrarie()
+  //collision_check()
+  //opponent_collision_check()
+  collision_checklibrarie()
+  opponent_collision_checklibrarie()
+  score()
+  scoring()
 }
 function show_the_ball(){
+  fill(250)
   circle(xBall, yBall, diameter)
 }
 function ball_movement(){
@@ -52,24 +59,32 @@ function edge_check(){
   }
 } 
 function show_the_platform(){
+  fill(255,0,0)
   rect(xPlatform, yPlatform, platformwidth, platformheight, platformcorner)
 }
-function show_opponent_platform(){
-  rect(xOpponentplatform, yOpponentplatform, platformwidth, platformheight, platformcorner)
-}
 function platform_movement(){
-  if (keyIsDown(LEFT_ARROW)){
-    xPlatform -= 10
+  if (keyIsDown(LEFT_ARROW) && xPlatform > 0){
+    xPlatform -= 10 
   }
-  if (keyIsDown(RIGHT_ARROW)){
+  if (keyIsDown(RIGHT_ARROW) && xPlatform < 425){
     xPlatform += 10
   }
 }
-function platform_move(){
-  opponentplatformspeed = xBall - xOpponentplatform - platformheight/2 - 50
-  xOpponentplatform += opponentplatformspeed
+function show_opponent_platform(){
+  fill(255)
+  rect(xOpponentplatform, yOpponentplatform, platformwidth, platformheight, platformcorner)
 }
-function collision_check(){
+function opponent_platform_move(){
+  opponentplatformspeed = xBall - xOpponentplatform - platformheight/2 - 72
+  xOpponentplatform += opponentplatformspeed
+  if(xOpponentplatform < 0){
+    xOpponentplatform = 0
+  }
+  if(xOpponentplatform > 425){
+    xOpponentplatform = 425
+  }
+}
+/*function collision_check(){
   if (xBall - radius < xPlatform + platformwidth 
     && yBall - radius < yPlatform + platformheight
     && yBall + radius > yPlatform
@@ -84,10 +99,32 @@ function opponent_collision_check(){
     && xBall + radius > xOpponentplatform){
     yBallspeed *= -1
   }
-}
+}*/
 function collision_checklibrarie(){ 
   collision = collideRectCircle(xPlatform, yPlatform, platformwidth, platformheight, xBall, yBall, radius)
   if(collision){
     yBallspeed *= -1
+  }
+}
+function opponent_collision_checklibrarie(){ 
+  collision = collideRectCircle(xOpponentplatform, yOpponentplatform, platformwidth, platformheight, xBall, yBall, radius)
+  if(collision){
+    yBallspeed *= -1
+  }
+}
+function score(){
+  textAlign(CENTER)
+  textSize(30)
+  fill(255,0,0)
+  text(myscores, 18, 300)
+  fill(300)
+  text(opponentscores, 480, 300)
+}
+function scoring(){
+  if (yBall > 590){
+    opponentscores += 1
+  }
+  if ( yBall < 12){
+    myscores += 1
   }
 }
